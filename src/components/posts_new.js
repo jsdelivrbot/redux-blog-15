e47fca0ea37d01b15ad7611a5d9373f0454cf4ce
Binh Nguyen <binhxn89@gmail.com>
2017-01-30 22:02:11 -0800

    Display individual post and added fetchPost action creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,24 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import validate from '../reducers/validate';
 
-const renderInput = field =>   // Define stateless component to render input and errors
+const renderField = field =>   // Define stateless component to render input and errors
   <div>
     <input {...field.input} className="form-control" type={field.type}/>
     {field.meta.touched &&
      field.meta.error &&
-     <span className="error">{field.meta.error}</span>}
+     <div className={`text-help ${field.meta.error ? 'error-text' : ''}`}>{field.meta.error}</div>}
   </div>
 
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const { handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -26,7 +38,7 @@ class PostsNew extends Component {
             name="title"
             type="text"
             className="form-control"
-            component={renderInput} />
+            component={renderField} />
         </div>
 
         <div className="form-group">
@@ -34,7 +46,7 @@ class PostsNew extends Component {
           <Field name="categories"
             type="text"
             className="form-control"
-            component={renderInput} />
+            component={renderField} />
         </div>
 
         <div className="form-group">
@@ -42,10 +54,11 @@ class PostsNew extends Component {
           <Field name="content"
             type="text"
             className="form-control"
-            component={renderInput} />
+            component={renderField} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     )
   }

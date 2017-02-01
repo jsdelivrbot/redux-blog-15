113e7797388d685253394a6415423e4d344f7717
Binh Nguyen <binhxn89@gmail.com>
2017-02-01 15:36:13 -0800

    Added bcrypt to hash server-side password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -4,25 +4,30 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  console.log(email, password);
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password' });
+  }
+
   // See if a user with a given email exists
   User.findOne({email}, (err, existingUser) => {
     if (err) return next(err);
 
     // If a user with email does exist, return error
-    if (existingUser) res.status(422).send({ error: 'Email is in use' });
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
 
     // If a user with email does NOT exist, create and save user record
     const user = new User({
-      email: email,
-      password: password
+      email,
+      password
     });
 
     user.save((err) => {
       if (err) return next(err);
 
       // Respond to request indicating the user was created
-      res.json(user);
+      res.json({ success: true });
     });
     
   });
--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
 const Schema = mongoose.Schema;
 
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
-})
+});
+
+
+// Note: unable to use arrow functions, does not hash password
+// On Save hook, encrypt password
+userSchema.pre('save', function (next) {
+  // Get access to user  model
+  const user = this;
+
+  // Generate salt then run callback
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) { return next(err); }
+
+    // Hash our password using the salt
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) { return next(err); }
+
+      // Overwrite plain text password with hashed password
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
